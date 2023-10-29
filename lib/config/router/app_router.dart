bcf80b4e3300d51a45b0c9bec04ebe819a482295
Franco Inostroza <franco.inostrozah@gmail.com>
2023-10-29 20:03:07 -0300

    :sparkles: (app-bar) added custom app bar

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,4 +1,4 @@
-import 'package:cinemapedia/config/presentation/screens/screen.dart';
+import 'package:cinemapedia/presentation/screens/screen.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
-import 'package:cinemapedia/config/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -38,12 +39,19 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     if (nowPlayingMovies.length == 0) return CircularProgressIndicator();
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(title: Text(movie.title));
-      },
+    return Column(
+      children: [
+        CustomAppBar(),
+        Expanded(
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final movie = nowPlayingMovies[index];
+              return ListTile(title: Text(movie.title));
+            },
+          ),
+        )
+      ],
     );
   }
 }
