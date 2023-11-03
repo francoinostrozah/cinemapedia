247eae3887536f30005aeb78aeb793fcdefea563
Franco Inostroza <franco.inostrozah@gmail.com>
2023-11-02 21:54:09 -0300

    :sparkles: (actors) added data source, repository and mapper actors

---
+++ lib/infrastructure/datasources/actor_moviedb_datasource.dart
@@ -0,0 +1,31 @@
+import 'package:cinemapedia/infrastructure/mappers/actor_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/credits_response.dart';
+import 'package:dio/dio.dart';
+import 'package:cinemapedia/config/constants/enviroment.dart';
+import 'package:cinemapedia/domain/datasources/actors_datasource.dart';
+import 'package:cinemapedia/domain/entities/actor.dart';
+
+class ActorMovieDbDatasource extends ActorsDatasource {
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Enviroment.theMovieDbKey,
+        'language': 'es-MX'
+      }));
+
+  @override
+  Future<List<Actor>> getActorByMovie(String movieId) async {
+    final response = await dio.get('/movie/$movieId/credits');
+
+    return _jsonToActors(response.data);
+  }
+
+  List<Actor> _jsonToActors(Map<String, dynamic> json) {
+    final castResponse = CreditsResponse.fromJson(json);
+    final List<Actor> actors = castResponse.cast
+        .map((cast) => ActorMapper.castToEntity(cast))
+        .toList();
+
+    return actors;
+  }
+}
---
+++ lib/infrastructure/mappers/actor_mapper.dart
@@ -0,0 +1,12 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/credits_response.dart';
+
+class ActorMapper {
+  static Actor castToEntity(Cast cast) => Actor(
+      id: cast.id,
+      name: cast.name,
+      profilePath: cast.profilePath != ''
+          ? 'https://image.tmdb.org/t/p/w500/${cast.profilePath}'
+          : 'https://qph.cf2.quoracdn.net/main-qimg-6d72b77c81c9841bd98fc806d702e859-lq',
+      character: cast.character);
+}
