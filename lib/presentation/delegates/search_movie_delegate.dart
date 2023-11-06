8b040f965e257a429dab9af5e50b62d8db7700b9
Franco Inostroza <franco.inostrozah@gmail.com>
2023-11-05 22:04:38 -0300

    :sparkles: (search) show movie calification

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -93,7 +94,23 @@ class _MovieItem extends StatelessWidget {
               ),
               (movie.overview.length > 100)
                   ? Text('${movie.overview.substring(0, 100)}...')
-                  : Text(movie.overview)
+                  : Text(movie.overview),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star_half_rounded,
+                    color: Colors.yellow.shade800,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    HumanFormat.number(movie.voteAverage, 1),
+                    style: textStyle.bodyMedium!
+                        .copyWith(color: Colors.yellow.shade900),
+                  )
+                ],
+              )
             ]),
           )
         ],
