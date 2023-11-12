fd25e541955182f84a28f83220b106147a33ae0a
Franco Inostroza <franco.inostrozah@gmail.com>
2023-11-12 12:55:46 -0300

    :fix: (search) fix redirect movie on search

--- lib/infrastructure/mappers/actor_mapper.dart
+++ lib/infrastructure/mappers/actor_mapper.dart
@@ -5,7 +5,7 @@ class ActorMapper {
   static Actor castToEntity(Cast cast) => Actor(
       id: cast.id,
       name: cast.name,
-      profilePath: cast.profilePath != ''
+      profilePath: (cast.profilePath != '' && cast.profilePath != null)
           ? 'https://image.tmdb.org/t/p/w500/${cast.profilePath}'
           : 'https://qph.cf2.quoracdn.net/main-qimg-6d72b77c81c9841bd98fc806d702e859-lq',
       character: cast.character);
